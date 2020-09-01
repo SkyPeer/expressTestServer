31393c8e33e6e3f6327c18846c3d8137eaef309a
Oleg Selivantsev <o.selivancev@gmail.com>
2020-09-01 23:50:14 +0300

    cacheAdded

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ let express = require("express"),
   bodyParser = require("body-parser"),
   server = http.createServer(app);
 
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -11,7 +14,21 @@ app.get("/test", function (request, response, next) {
   response.send("test - ok!");
 });
 
-app.get("*", function (req, res) {
+app.get("/add", function (request, response, next) {
+  const val = Math.random();
+  // const ttl = undefined; //TTL in Seconds
+  const ttl = 10; //TTL in 10 Seconds
+  myCache.set("test", val, ttl || null);
+  response.send("added");
+});
+
+app.get("/get", function (request, response, next) {
+  const data = myCache.get("test");
+  console.log("data", data);
+  response.send("reques-ok!");
+});
+
+app.get("*", function (req, response) {
   res.send("HELLO");
 });
 
